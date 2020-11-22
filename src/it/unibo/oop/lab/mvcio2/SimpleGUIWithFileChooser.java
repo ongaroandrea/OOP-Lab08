9b883fc9a467b12390017c0fdd9f70ba83941e53
Andrea Ongaro <andrea.ongaro2@studio.unibo.it>
2020-11-22 09:28:25 +0100

    End Fourth Ex

--- src/it/unibo/oop/lab/mvcio2/SimpleGUIWithFileChooser.java
+++ src/it/unibo/oop/lab/mvcio2/SimpleGUIWithFileChooser.java
@@ -1,11 +1,88 @@
 package it.unibo.oop.lab.mvcio2;
 
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+
+import it.unibo.oop.lab.mvcio.Controller;
+
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.filechooser.FileNameExtensionFilter;
+
 /**
  * A very simple program using a graphical interface.
  * 
  */
 public final class SimpleGUIWithFileChooser {
 
+    private final JFrame frame = new JFrame("Simple GUI With File Chooser");
+
+    private SimpleGUIWithFileChooser(final Controller c) {
+        final JPanel canvas = new JPanel();
+        canvas.setLayout(new BorderLayout());
+
+        final JPanel browsePanel = new JPanel();
+        browsePanel.setLayout(new BorderLayout());
+        final JTextField txtField = new JTextField(c.getPathCurrentFile());
+        txtField.setEditable(false);
+        txtField.setColumns(100); //NOT RESPONSIVE
+        browsePanel.add(txtField, BorderLayout.LINE_START);
+        final JButton btnBrowse = new JButton("Browse...");
+
+        btnBrowse.addActionListener(new ActionListener() {
+            public void actionPerformed(final ActionEvent e) {
+                final JFileChooser chooser = new JFileChooser();
+                chooser.setSelectedFile(c.getCurrentFile());
+                FileNameExtensionFilter filter = new FileNameExtensionFilter("Text Files", "txt");
+                chooser.setFileFilter(filter);
+                final var result = chooser.showSaveDialog(chooser);
+                if (result == JFileChooser.APPROVE_OPTION) {
+                    try {
+                        c.setCurrentFile(chooser.getSelectedFile());
+                        txtField.setText(c.getPathCurrentFile());
+                    } catch (IOException e1) {
+                        e1.printStackTrace();
+                    }
+                } else {
+                    System.out.println("Nothing to do");
+                }
+            }
+        });
+        browsePanel.add(btnBrowse, BorderLayout.LINE_END);
+
+        canvas.add(browsePanel, BorderLayout.NORTH);
+        final JTextArea textArea = new JTextArea();
+        final JButton btnSave = new JButton("Save");
+        canvas.add(textArea);
+        canvas.add(btnSave, BorderLayout.PAGE_END);
+        btnSave.addActionListener(new ActionListener() {
+            public void actionPerformed(final ActionEvent e) {
+                try {
+                    c.saveContent(textArea.getText());
+                } catch (IOException e1) {
+                    e1.printStackTrace();
+                }
+            }
+        });
+        frame.setContentPane(canvas);
+
+        final Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
+        final int sw = (int) screen.getWidth();
+        final int sh = (int) screen.getHeight();
+        frame.setSize(sw / 2, sh / 2);
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLocationByPlatform(true);
+        frame.setVisible(true);
+    }
     /*
      * TODO: Starting from the application in mvcio:
      * 
@@ -31,5 +108,7 @@ public final class SimpleGUIWithFileChooser {
      * update the UI: in this example the UI knows when should be updated, so
      * try to keep things separated.
      */
-
+    public static void main(final String... args) {
+        new SimpleGUIWithFileChooser(new Controller());
+     } 
 }
